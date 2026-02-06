d35baeeaf4011a2ca1a3c53c174c7428c78e4bfc
Nastya <morozn303@gmail.com>
2026-02-06 13:09:10 +0200

    modified site a bit and added new albums

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleBurgerMenu() {
+    let burgerMenu = document.getElementById("burgerMenu");
+    burgerMenu.classList.toggle("burgerMenuHidden");
+}
+
+function toggleCD() {
+    let cdGenres = document.getElementById("cd-genres");
+    cdGenres.classList.toggle("formatHidden");
+}
+
+function toggleCS() {
+    let csGenres = document.getElementById("cs-genres");
+    csGenres.classList.toggle("formatHidden");
+}
+
+function toggleLP() {
+    let lpGenres = document.getElementById("lp-genres");
+    lpGenres.classList.toggle("formatHidden");
+}
